f4d86bbea0f3948490ad693e252eab56bde45ca6
Ahmad Rifki Ayala <ayalarifki@gmail.com>
2024-09-04 15:37:40 +0700

    get href location

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 import { onClick } from 'https://cdn.jsdelivr.net/gh/jscroot/lib@0.0.4/element.js';
-import { fullPath, redirect } from 'https://cdn.jsdelivr.net/gh/jscroot/lib@0.0.4/url.js';
+import { redirect } from 'https://cdn.jsdelivr.net/gh/jscroot/lib@0.0.4/url.js';
 
 onClick('loginulbi', loginUlbi);
 function loginUlbi() {
   console.log('loginulbi');
-  setCookieWithExpireHour('redirect', fullPath(), 1);
+  setCookieWithExpireHour('redirect', window.location.href, 1);
     redirect('https://login.ulbi.ac.id');
 }
 
