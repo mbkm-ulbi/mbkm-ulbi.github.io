3979859566cf818fad98183d70537966e98f0838
Ahmad Rifki Ayala <ayalarifki@gmail.com>
2024-09-04 15:30:38 +0700

    update main js

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 import { onClick } from 'https://cdn.jsdelivr.net/gh/jscroot/lib@0.0.4/element.js';
-import { setCookieWithExpireHour } from 'https://cdn.jsdelivr.net/gh/jscroot/lib@0.0.4/cookie.js';
 import { fullPath, redirect } from 'https://cdn.jsdelivr.net/gh/jscroot/lib@0.0.4/url.js';
 
 onClick('loginulbi', loginUlbi);
@@ -8,3 +7,15 @@ function loginUlbi() {
   setCookieWithExpireHour('redirect', fullPath(), 1);
     redirect('https://login.ulbi.ac.id');
 }
+
+export function setCookieWithExpireHour(cname, cvalue, exhour) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exhour * 60 * 60 * 1000));
+    let expires = "expires=" + d.toUTCString();
+    
+    // Replace 'example.com' with your domain name
+    let domain = "domain=.ulbi.ac.id"; 
+  
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";" + domain + ";path=/";
+  }
+  
