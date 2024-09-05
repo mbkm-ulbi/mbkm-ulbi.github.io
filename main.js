32e98cee46fbb54f54d26996ab9088842c0f4522
Ahmad Rifki Ayala <ayalarifki@gmail.com>
2024-09-05 20:01:26 +0700

    update func button login and logout for remove token

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import { onClick } from 'https://cdn.jsdelivr.net/gh/jscroot/lib@0.0.4/element.js';
 import { redirect } from 'https://cdn.jsdelivr.net/gh/jscroot/lib@0.0.4/url.js';
+import { getCookie } from 'https://cdn.jsdelivr.net/gh/jscroot/lib@0.0.4/cookie.js';
 
 onClick('loginulbi', loginUlbi);
 function loginUlbi() {
@@ -18,3 +19,24 @@ export function setCookieWithExpireHour(cname, cvalue, exhour) {
   
     document.cookie = cname + "=" + cvalue + ";" + expires + ";" + domain + ";path=/";
   }
+
+function updateLoginButton() {
+  let token = getCookie("login");
+  let loginButton = document.getElementById("loginulbi");
+  if (token !== "") {
+    loginButton.textContent = "Logout";
+    loginButton.id = "logout";
+    loginButton.onclick = function() {
+      logout();
+    }
+  } 
+}
+
+function logout() {
+  document.cookie = "loginToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+  window.location.href = "/";
+}
+
+window.onload = function() {
+  updateLoginButton();
+};
