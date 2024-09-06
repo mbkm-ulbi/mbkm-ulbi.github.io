2c45fb376509771ee1c75d916c45ea45f8489c89
GilangAndhika <gryzlegrizz@gmail.com>
2024-09-06 14:43:10 +0700

    Refaktor login dan  logout button visibility logic

--- main.js
+++ main.js
@@ -15,21 +15,20 @@ function setCookieWithExpireHour(cname, cvalue, exhour) {
   d.setTime(d.getTime() + (exhour * 60 * 60 * 1000));
   let expires = "expires=" + d.toUTCString();
   document.cookie = `${cname}=${cvalue};${expires};domain=.ulbi.ac.id;path=/`;
+  console.log(`Cookie set: ${cname}=${cvalue}`);
 }
 
 // Fungsi untuk delete cookie
 function deleteCookie(name) {
   document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; domain=.ulbi.ac.id;path=/`;
   document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/`;
+  console.log(`Cookie deleted: ${name}`);
 }
 
 // Fungsi untuk logout
 function logout() {
   console.log('Logout button clicked');
   deleteCookie('login');
-  console.log("Cookie 'login' deleted");
-
-  // Hapus cookie redirect
   deleteCookie('redirect');
   window.location.href = "/";
 }
@@ -42,15 +41,21 @@ function updateLoginButton() {
 
   console.log("Memperbarui visibilitas tombol. Token:", token);
 
+  if (loginButton) {
+    // Jika token ada, maka tombol login dihide
+    loginButton.hidden = !!token;
+  }
+  
+  if (logoutButton) {
+    // Jika token tidak ada, maka tombol logout dihide
+    logoutButton.hidden = !token;
+  }
+
   if (token) {
-    if (loginButton) loginButton.hidden = true;
-    if (logoutButton) {
-      logoutButton.hidden = false;
-      onClick('logoutulbi', logout); // Bind fungsi logout untuk logout button
-    }
+    console.log("User is logged in. Binding logout function.");
+    onClick('logoutulbi', logout); // Bind fungsi logout untuk logout button
   } else {
-    if (loginButton) loginButton.hidden = false;
-    if (logoutButton) logoutButton.hidden = true;
+    console.log("User is not logged in. Binding login function.");
     onClick('loginulbi', loginUlbi); // Bind fungsi login untuk login button
   }
 }
